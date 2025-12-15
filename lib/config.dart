9f139bc0e591ba86ea6645db5c4f3bed7c61a18f
Ivan Bikhe <90923563+bikhe@users.noreply.github.com>
2025-12-15 10:41:45 +0300

    зачистка config.py

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   // Для эмулятора Android используйте 10.0.2.2
   // Для реального устройства используйте IP вашего компьютера (например, 192.168.1.X)
-  static const String baseUrl = 'http://77.221.156.93:8000';
+  static const String baseUrl = 'http://0.0.0.0:8000';
   static const String apiUrl = '$baseUrl/api';
 }
